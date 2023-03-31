61ad7b48b680f2edcbcdf8352e161d7b8275a06c
Aldhi Albadri <aldhi.albadri@gmail.com>
2023-03-31 10:45:21 +0700

    perubahan baru lagi

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
-
-ECHO 'terserah';
-echo "terserah";
-
-echo "ini perubahan";
-
+$nama = "Budi";
+echo "<h1>".$nama."</h1>"
 ?>
+<h1> <?php echo $nama  ?> </h1>
+</body>
+</html>
