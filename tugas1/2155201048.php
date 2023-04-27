111f882701a54f065d33283c51102e856045223d
putri eka oktapiya <putriekaoktapiya@gmail.com>
2023-04-28 03:41:20 +0700

    tugas 1 membuat form

---
+++ tugas1/2155201048.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<form>
+      <label for="nama">Nama:</label>
+      <input type="text" id="nama" name="nama"><br></br>
+<form method= 'POST' action=' '>
+      <h3>gender</h3>
+      <h4><select name="gender">
+          <option value="laki-laki">laki-laki</option>
+          <option value="perempuan">perempuan</option>
+          <option value="lainnya">lainnya</option>
+      </h4>
+</body>
+</html>
+
