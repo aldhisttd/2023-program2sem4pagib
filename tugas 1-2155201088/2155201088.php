2991748c740a9b953397299d407b58dec65f05df
siti annisa maulidya <sitiannisa maulidya21@gmail.com>
2023-04-28 17:35:49 +0700

    form input mahasiswa

---
+++ tugas 1-2155201088/2155201088.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FORM MAHASISWA</title>
+</head>
+<body>
+    <h1 class="text-center">FORM MAHASISWA</h1>
+    <form id="form" method="POST">
+    <div class="row">
+        <label> Name :</label>
+        <input type="text" name="nama" class= "from-control" placeholder="Masukkan Nama Lengkap">
+    </div>
+    </form>
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="form-grup">
+                <label>Gander : </label>
+                <br>
+                <input type="radio" name="jk" value="MALE">Male
+                <br>
+                <input type="radio" name="jk" value="FIMALE">Fimale
+
+            </div>
+        </div>   
+    </div> 
+    
+</body>
+</html>
