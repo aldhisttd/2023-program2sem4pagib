c02854cf69db4a7e16d7dee4ab3fd410ef9dffbf
Angelina Paskah <angelinasimanjuntak200403@gmail.com>
2023-04-28 16:06:40 +0700

    form mahasiswa

---
+++ tgs-1-2155201061/TGS-01-2155201061.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FORM MAHASISWA</title>
+</head>
+<body>
+    <h1 class="text-center">FORM MAHASISWA</h1>
+    <form id="form" method="POST">
+     <div class="row">
+        <label>Nama Mahasiswa :</label>
+        <input type="text" name="nama" class="from-control" placeholder="Masukkan Nama Lengkap">   
+     </div>
+    </Form>
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="form-grup">
+                <label>Jenis Kelamin :</label>
+                <br>
+                <input type="radio" name="jk" value="Laki-Laki">Laki-Laki
+                <br>
+                <input type="radio" name="jk" value="Perempuan">Perempuan
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
