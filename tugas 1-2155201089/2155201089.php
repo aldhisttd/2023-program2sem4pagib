9748e2999b0b3d0835e40ba43680fb4617463bf9
siti annisa maulidya <sitiannisa maulidya21@gmail.com>
2023-04-28 14:09:43 +0700

    membuat tugas form

---
+++ tugas 1-2155201089/2155201089.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<form>
+    <label for="nama">Nama:</label>
+    <input type="text" id="nama" name="nama"><br></br>
+<from method= 'POST' action=''>
+    <h2>gender</h2>
+    <h2><select name="gender">
+        <option value="laki-laki">laki-laki</option>
+        <option value="perempuan">perempuan</option>
+        <option value="lainnya">lainnya</option>
+    </h2>
+</form>
+</html>
