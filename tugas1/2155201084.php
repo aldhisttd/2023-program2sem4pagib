a5328350dd1e83617f00405cf599829eb27dae77
Agustiana Wardani <agustianawardani95@gmail.com>
2023-04-28 20:51:01 +0700

    tugas 1 membuat form

---
+++ tugas1/2155201084.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <from>
+        <label for="nama">Nama:</label>
+        <input type="text" id="nama" name="nama"><br></br>
+        <from method= 'POST' action=' '>
+            <h3>gender</h3>
+            <h4><select name="gender">
+                <option value="laki-laki">laki-laki</option>
+                <option value="perempuan">perempuan</option>
+                <option value="lainya">lainya</option>
+</h4>
+</body>
+</html>
