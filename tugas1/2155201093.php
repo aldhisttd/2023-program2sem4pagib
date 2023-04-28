b8a93efe4c7b63e3f5a022aaff4c8b76764093de
ayukumaladewi <ayuk50362@gmail.com>
2023-04-28 23:30:46 +0700

    membuat form mahasiswa

---
+++ tugas1/2155201093.php
@@ -0,0 +1,12 @@
+<html>
+<form>
+      <label for="nama">Nama:</label>
+      <input type="text" id="nama" name="nama"><br></br>
+<form method= 'POST' action=' '>
+      <h4>gender</h4>
+      <h4><select name="gender">
+          <option value="laki-laki">laki-laki</option>
+          <option value="perempuan">perempuan</option>
+      </h4>
+</body>
+</html>
