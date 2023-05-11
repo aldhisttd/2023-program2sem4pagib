ae51a4a657b007a1491cd5997c15e4f86d03e377
FanyFadhilah <fdhlhfny@gmail.com>
2023-05-11 23:09:10 +0700

    tugas1

---
+++ tugas1/2155201053.php
@@ -0,0 +1,14 @@
+<html>
+<form>
+  <label for="nama">Nama:</label>
+  <input type="text" id="nama" name="nama"><br><br>
+  
+  <label for="gender">Gender:</label>
+  <select id="gender" name="gender">
+    <option value="laki-laki">Laki-laki</option>
+    <option value="perempuan">Perempuan</option>
+  </select><br><br>
+
+  <input type="submit" value="Submit">
+</form>
+</html>
