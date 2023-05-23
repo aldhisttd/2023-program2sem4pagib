e404f9d15601c1bb734da46cae9b0cb39ce9e640
Agustiana Wardani <agustianawardani95@gmail.com>
2023-05-23 10:50:54 +0700

    tugas2 membuat form

--- tugas2/2155201082/proses.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (isset ($_POST["send"])){
-    echo$_POST['nama'].',';
-    echo$_POST['gender'];
-}
-?>
---
+++ tugas2/tugas2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=f, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action="proses.php">
+        <label for="nama">Nama:</label>
+        <input type="text" id="nama" name="nama"></input>
+        <label for="gender">Gender:</label>
+        <input type="text" id="gender" name="gender"></input>
+        <button type="submit" name="simpan" value="1">simpan</button>
+    </form>
+</body>
+</html>
