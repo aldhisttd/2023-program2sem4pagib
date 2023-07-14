552b34a54c1c0cfc207c19016e847510bc9d381d
Acer V5 <resturamadan658@gmail.com>
2023-07-14 19:50:46 +0700

    quiz Denihendriawan

---
+++ quiz/01_login/2155201075/user.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+// jika belum login, redirect ke halaman login
+if (!isset($_SESSION['user_login'])) {
+    header('location:login.php');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+
+<body>
+    <h1>Selamat Datang, user!</h1>
+    <a href="logout.php">Logout</a>
+    <h3>Ini halaman user.</h3>
+
+</body>
+</html>
