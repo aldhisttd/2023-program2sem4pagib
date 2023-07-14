c451ff0bf7d26dfd37f0ff73eaec700daf7c7218
iraahdani <fdhlhfny@gmail.com>
2023-07-14 08:45:56 -0700

    quiz/01_login/iraahdanisiregar

---
+++ quiz/01_login/2155201083/admin.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+// jika belum login, redirect ke halaman login
+if (!isset($_SESSION['adminlogin'])) {
+    header('location:login.php');
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+
+<body>
+    <h1>Selamat Datang,admin!
+    </h1>
+    <h3>ARIGATO </h3>
+    <a href="logout.php">Logout</a>
+    
+
+</body>
+
+</html>
