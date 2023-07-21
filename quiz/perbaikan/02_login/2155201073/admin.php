5a78a0870bed4410df876875097164fb33e67286
Mythicomyia <irhasmubaraq28@gmail.com>
2023-07-21 20:35:33 +0700

    perbaikan login multi akun no db fitrahul juma sawika

---
+++ quiz/perbaikan/02_login/2155201073/admin.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+// jika belum login, redirect ke halaman login
+if (!isset($_SESSION['is_login'])) {
+    header('location:login.php');
+}
+if ($_SESSION['rules'] != 'admin') {
+    header('location:' . $_SESSION['rules'] . '.php');
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+
+<body>
+    <h1>Selamat Datang, <?= $_SESSION['rules'] ?> !</h1>
+
+    <a href="logout.php">Logout</a>
+
+    <h3>Ini halaman <?= $_SESSION['rules'] ?>.</h3>
+</body>
+
+</html>
