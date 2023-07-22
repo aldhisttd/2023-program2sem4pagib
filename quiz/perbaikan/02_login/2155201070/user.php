363afe982b82a392c10e6ca0d70d83ad6370ff8e
sitijamila32 <sitiiijamilah32@gmail.com>
2023-07-22 13:09:19 +0700

    perbaikan quiz 2 login siti jamila

---
+++ quiz/perbaikan/02_login/2155201070/user.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['is_login'])) {
+    header('location:index.php');
+}
+if ($_SESSION['role'] == 'admin') {
+    header('location:admin.php');
+}
+if ($_SESSION['role'] == 'karyawan') {
+    header('location:karyawan.php');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+
+<body>
+
+    <p>Hallo <?= $_SESSION['name']; ?>!</p>
+    <h1>Halaman <?= $_SESSION['role']; ?></h1>
+    <p><a href="logout.php">Logout</a></p>
+</body>
+
+</html>
