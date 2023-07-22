880f806f757b246143a10a70a69c2bc21f58524c
Angelina Paskah <angelinasimanjuntak200403@gmail.com>
2023-07-22 20:46:41 +0700

    perbaikan login angelina

---
+++ quiz/perbaikan/02_login/2155201061/login.php
@@ -0,0 +1,73 @@
+<?php
+session_start();
+
+if (isset($_POST['masuk'])) {
+    $user = $_POST['username'];
+    $pass = $_POST['password'];
+
+    $userAdmin = 'admin';
+    $passAdmin = '123';
+
+    $userUser = 'user';
+    $passUser = '1234';
+
+    $userKaryawan = 'karyawan';
+    $passKaryawan = '12345';
+
+    if ($user == $userAdmin && $pass == $passAdmin) {
+        $_SESSION['masuk'] = $user;
+        $_SESSION['role'] = 'admin';
+        header('location:admin.php');
+    }
+   
+    if ($user == $userUser && $pass == $passUser) {
+        $_SESSION['masuk'] = $user;
+        $_SESSION['role'] = 'user';
+        header('location:user.php');
+    }
+    if ($user == $userKaryawan && $pass == $passKaryawan) {
+        $_SESSION['masuk'] = $user;
+        $_SESSION['role'] = 'karyawan';
+        header('location:karyawan.php');
+    } else {
+        echo 'Your password and username are incorrect! ';
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Login</title>
+</head>
+
+<body>
+
+    <h1>Form Login</h1>
+    <form action="" method="post">
+        <table>
+            <tr>
+                <td>Username</td>
+                <td>:</td>
+                <td><input type="text" name="username"></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td>:</td>
+                <td><input type="password" name="password"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td><button type="submit" name="masuk">Masuk</button></td>
+            </tr>
+        </table>
+    </form>
+
+</body>
+
+</html>
