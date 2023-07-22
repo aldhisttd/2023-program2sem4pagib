34f898b2642bd82864f64db1f91afe2ae4c2fb42
Acer V5 <resturamadan658@gmail.com>
2023-07-22 23:57:11 +0700

    perbaikan login deni hendriawan

---
+++ quiz/perbaikan/02_login/2155201075/user.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['masuk'])) 
+{header('location: login.php');}
+
+if ($_SESSION['role'] == 'admin') 
+{header('location: admin.php');}
+if ($_SESSION['role'] == 'karyawan')
+{header('location: karyawan.php');}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <p>Anda telah login sebagai <b><?= $_SESSION['masuk']; ?></b></p>
+    <center>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <h1>WELCOME User</h1>
+    <font color=”blue”><h4>Halaman ini hanya dapat diakses oleh user user</h4></font>
+
+    <p><button><a href="logout.php">Logout</a></button></p>
+    
+</body>
+</html>
