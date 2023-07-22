0a44d3ab4db6f27db6e0fe91ed5fecb01c48c0c8
sitijamila32 <sitiiijamilah32@gmail.com>
2023-07-22 17:41:55 +0700

    perbaikan crud siti jamilah

---
+++ quiz/perbaikan/01_crud/2155201070/act/ac_edit.php
@@ -0,0 +1,33 @@
+<?php
+
+// ambil semua value dari form
+$isbn = $_POST['isbn'];
+$judul = $_POST['judul'];
+$kategori = $_POST['kategori'];
+$stok = $_POST['stok'];
+
+// koneksi
+include "koneksi.php";
+if ($_FILES['file']['file'] != "") {
+
+    // ambil nama photo lama
+    $q = mysqli_query($koneksi, "SELECT file FROM tb_buku WHERE isbn='$isbn'");
+    $dt = mysqli_fetch_array($q);
+    $file = $dt['file'];
+
+    // hapus photo lama
+    unlink('../file/' . $file);
+
+    // upload photo baru
+    $file = $_FILES['file']['file'];
+    move_uploaded_file($_FILES['file']['tmp_name'], '../file/' . $judul);
+
+
+    // update nama photo ke photo baru
+    mysqli_query($koneksi, "UPDATE tb_buku SET file='$judul' WHERE isbn='$isbn'");
+}
+
+// update dengan condition
+mysqli_query($koneksi, "UPDATE tb_buku SET judul='$judul', stok='$stok', kategori='$kategori', file='$file' WHERE isbn='$isbn'");
+// pindah kehalaman data.php
+header("location:../data.php");
