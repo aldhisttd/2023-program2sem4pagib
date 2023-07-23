ff574c9c2c55307b13eea4fcdbfc5ed9f83309e1
putri eka oktapiya <putriekaoktapiya@gmail.com>
2023-07-24 06:43:09 +0700

    quiz logindb putri eka

---
+++ quiz/03_logindb/2155201048/logindb.php
@@ -0,0 +1,41 @@
+<?php
+include "action/login.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Login</title>
+</head>
+<body>
+    <form action="action/login.php" method="POST">
+
+        <?php if (isset($error)) : ?>
+
+        <p>username atau password error </p>
+
+        <?php endif; ?>
+
+        <table>
+            <tr>
+                <td>Username</td>
+                <td>:</td>
+                <td><input type="text" name="userform"></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td>:</td>
+                <td><input type="password" name="passform"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td><input type="submit" name="btnlogin" value="Login"></td>
+            </tr>
+        </table>
+
+    </form>
+</body>
+</html>
