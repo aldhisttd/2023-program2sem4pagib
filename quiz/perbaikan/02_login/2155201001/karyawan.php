d7e8c0f5b88db96883e60305cdb5392234dbaa2c
Loren22 <gamer224488@mail.com>
2023-07-24 12:55:47 +0700

    perbaikan tugas login lorentadha

---
+++ quiz/perbaikan/02_login/2155201001/karyawan.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['masuk'])) {header('location: login.php');}
+
+if ($_SESSION['role'] == 'user') {header('location: user.php');}
+if ($_SESSION['role'] == 'admin') {header('location: admin.php');}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <p>Anda telah login sebagai <b><?= $_SESSION['masuk']; ?></b></p>
+    <center>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <h1>Selamat Datang Di Halaman Karyawan</h1>
+    <b>Halaman ini hanya dapat diakses oleh user karyawan</b>
+    <p><button><a href="logout.php">Logout</a></button></p>
+    
+</body>
+</html>
