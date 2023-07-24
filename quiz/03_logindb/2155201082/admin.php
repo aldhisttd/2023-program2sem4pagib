39d528dff93f2d257cc3ad34490d77e573491513
Dita-Rizki-Fitria <ditaf2512@gmail.com>
2023-07-24 23:17:39 +0700

    quiz logindb dita

---
+++ quiz/03_logindb/2155201082/admin.php
@@ -0,0 +1,38 @@
+<?php 
+session_start();
+
+if (!isset($_SESSION['sudah_login'])) {
+    header('location: ../form.php');
+    exit();
+}
+
+// Periksa peran pengguna
+// Periksa peran pengguna
+if ($_SESSION['username'] === 'admin') {
+    // User adalah admin, lanjutkan ke halaman admin
+    // ...
+} elseif ($_SESSION['username'] === 'user') {
+    // User adalah user, lanjutkan ke halaman user
+    header('location: user.php');
+    exit();
+} else {
+    // User bukan admin atau user yang valid
+    header('location: karyawan.php');
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+<body>
+<h1>Halaman Admin</h1>
+    <p>Login sebagai : <?php echo $_SESSION['username'] ?></p>
+    <p><a href="logout.php">Logout</a></p>
+</body>
+</html>
